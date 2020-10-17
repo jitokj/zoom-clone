1b53d87cf6f99d0463f5d30d51abbcdf0b896ac2
jito <jitokj3@gmail.com>
2020-10-17 23:42:06 +0530

    added video capture
    video capture from browser

---
+++ public/script.js
@@ -0,0 +1,22 @@
+const videogrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+//let myVideoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    //myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+  });
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videogrid.append(video);
+};
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const server = require("http").Server(app);
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.status(200).send("hello world");
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.roomId });
 });
 
 server.listen(3000, () => {
