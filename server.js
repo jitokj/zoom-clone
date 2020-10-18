22dc12240f4ddb92b8d30342a392d209797868db
jito <jitokj3@gmail.com>
2020-10-18 14:39:36 +0530

    added peerjs call
    socketio and peerjs modules included

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
 const path = require("path");
+const { POINT_CONVERSION_UNCOMPRESSED } = require("constants");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -15,6 +24,13 @@ app.get("/", (req, res) => {
 
 app.get("/:roomId", (req, res) => {
   res.render("room", { roomId: req.params.roomId });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
 });
 
 server.listen(3000, () => {
